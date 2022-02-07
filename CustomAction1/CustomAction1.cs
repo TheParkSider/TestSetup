36d1561e818b3fc589d8309828a9d86bbcf8df75
TheParkSider <theparksider@yahoo.co.jp>
2022-02-07 14:16:56 +0900

    プロジェクト ファイルを追加します。

---
+++ CustomAction1/CustomAction1.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections;
+
+namespace CustomAction1
+{
+    [RunInstaller(true)]
+    public class CustomAction1 : System.Configuration.Install.Installer
+    {
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            MessageBox.Show(GetType().Name + ": Install");
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            MessageBox.Show(GetType().Name + ": Commit");
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+            MessageBox.Show(GetType().Name + ": Rollback");
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            MessageBox.Show(GetType().Name + ": Uninstall");
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            base.OnBeforeInstall(savedState);
+            MessageBox.Show(GetType().Name + ": OnBefore");
+            throw new System.Configuration.Install.InstallException("Errorだあああああ");
+
+        }
+    }
+}
---
+++ CustomAction2/CustomAction2.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections;
+
+namespace CustomAction2
+{
+    [RunInstaller(true)]
+    public class CustomAction2 : System.Configuration.Install.Installer
+    {
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            MessageBox.Show(GetType().Name + ": Install");
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            MessageBox.Show(GetType().Name + ": Commit");
+        }
+
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+            MessageBox.Show(GetType().Name + ": Rollback");
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+            MessageBox.Show(GetType().Name + ": Uninstall");
+        }
+    }
+}
